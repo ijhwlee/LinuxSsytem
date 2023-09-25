69667d89e80f1dd33401c7dfcc2481a2ff8d9c42
Lee Hyung Won <ijhwlee@gmail.com>
2023-09-25 12:42:11 +0900

    Create ch4_1.c

---
+++ Chap04/ch4_1.c
@@ -0,0 +1,20 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+  int fd;
+  mode_t mode;
+  mode = 0644;
+  fd = open("comsi.txt", O_CREAT, mode);
+  if(fd == -1)
+  {
+    perror("Create");
+    exit(-1);
+  }
+  close(fd);
+  return 0;
+}
